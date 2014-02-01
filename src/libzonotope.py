f5db41fa773b014e5de3fe9609b188a6dc80a5eb
Hörður Freyr Yngvason <hordurf@gmail.com>
2014-02-01 00:30:39 -0800

    Added halfspace computations to the python interface (*untested*)

--- src/libzonotope.py
+++ src/libzonotope.py
@@ -1,10 +1,47 @@
 from ctypes import *
 
-libzonotope_cdll = CDLL('./libzonotope.so')
+libzonotope_cdll = CDLL('./libzonotope_extern.so')
 
-def zonotope_volume(generators):
-    d = len(generators[0])
-    n = len(generators)
+#
+# Lookup tables for dynamic typing
+#
+
+cdll_zonotope_volume_fn = {
+    c_long: libzonotope_cdll.zonotope_volume_long
+}
+
+cdll_zonotope_halfspaces_fn = {
+    c_long: libzonotope_cdll.zonotope_halfspaces_long
+}
+
+#
+# Utility functions
+#
+
+def _dimensions(generators):
+    return len(generators[0]), len(generators)
+
+def _flatten_to_ctypes_array(generators, T=c_long):
+    """
+    Flatten generators to a C-array of type T
+    """
     _generators = [x for column in generators for x in column]
-    _generators = (c_long * (n*d))(*_generators)
-    return libzonotope_cdll._zonotope_volume(d,n, _generators)
+    _generators = (T * (n*d))(*_generators)
+
+#
+# Exported interface
+#
+
+def zonotope_volume(generators, T=c_long):
+    d, n = dimensions(generators)
+    _generators = _flatten_to_ctypes_array(generators)
+    return cdll_zonotope_volume_fn[T](d, n, _generators)
+
+def zonotope_halfspaces(generators, T=c_long):
+    """
+    Return a generator to the list of halfspaces of the zonotope
+    """
+    d, n = dimensions(generators)
+    _generators = _flatten_to_ctypes_array(generators)
+    _halfspaces = cdll_zonotope_halfspaces_fn[T](d, n, _generators)
+    return ([a[i] for i in xrange(k, k+d+1)] for k in xrange(0, n*(d+1), d+1))
--- src/zonotope_volume_extern.cpp
+++ src/zonotope_volume_extern.cpp
@@ -1,21 +1,68 @@
 #include "../include/zonotope_volume.hpp"
+#include "../include/zonotope_halfspaces.hpp"
 
-extern "C" {
-  long _zonotope_volume(int d, int n, long* _generators) {
 
-    std::vector<std::vector<long> >
-      generators (n, std::vector<long> (d));
-    
-    for ( int k = 0; k < n; ++k ) {
-      for ( int i = 0; i < d; ++i ) {
-        generators[k][i] = _generators[i + k*n];
+/**
+ * Convert the column-major m-by-n matrix `arr` from a flat array to
+ * an m-by-n matrix. If `transpose == true`, then instead output the
+ * transpose of `arr`. The contents of `mat` are entirely overwritten
+ * (along with its dimensions).
+ * 
+ */
+template <typename T>
+static void array_to_matrix_2(const int m,
+                       const int n,
+                       const bool transpose,
+                       const T* arr,
+                       std::vector<std::vector<T> >& mat) {
+
+  mat = std::vector<std::vector<T> > (m, std::vector<T> (n));
+
+  if ( transpose ) {
+    for ( int i = 0; i < m; ++i ) {
+      for ( int j = 0; j < n; ++j ) {
+        mat[i][j] = arr[j + i*n];
       }
     }
-    return zonotope_volume(generators);
+  } else {
+    for ( int i = 0; i < m; ++i ) {
+      for ( int j = 0; j < n; ++j ) {
+        mat[i][j] = arr[i + j*m];
+      }
+    }
   }
 }
 
 
+/**
+ *
+ * Exported interface
+ * 
+ */
 
+extern "C" {
+  long zonotope_volume_long(int d, int n, long* generators) {
+    std::vector<std::vector<long> > _generators;
+    array_to_matrix_2(n, d, true, generators, _generators);
+    return zonotope_volume(_generators);
+  }
 
+  long* zonotope_halfspaces_long(int d, int n, long* generators) {
+    std::vector<std::vector<long> > _generators;
+    array_to_matrix_2(n, d, true, generators, _generators);
+    std::set<Hyperplane<long> > _halfspaces;
+    zonotope_halfspaces(_generators, _halfspaces);
+    
+    long* halfspaces = new long[(d+1) * _halfspaces.size()];
 
+    std::set<Hyperplane<long> >::size_type count = 0;
+    for ( const Hyperplane<long>& h : _halfspaces ) {
+      halfspaces[count++] = h.offset;
+      for ( int i = 0; i < d; ++i ) {
+        halfspaces[count++] = h.normal[i];
+      }
+    }
+    
+    return halfspaces;
+  }
+}
