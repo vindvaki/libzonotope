7f0cb8faefd5c5c5aec4bc2da6e97ac17d2c3c56
Hörður Freyr Yngvason <hordurf@gmail.com>
2014-03-13 00:49:07 -0700

    Vector_t as template

--- include/standardize_vector.hpp
+++ include/standardize_vector.hpp
@@ -2,29 +2,37 @@
 #define STANDARDIZE_VECTOR_HPP_
 
 #include <vector>
+#include <array>
 #include <gmpxx.h>
 
-template<typename Number_t>
-inline Number_t standardize_vector ( std::vector<Number_t>& );
+namespace zonotope {
+
+template <typename Number_t>
+inline void _gcd(Number_t& a, const Number_t& b);
+
+template <>
+inline void _gcd<mpz_class>(mpz_class& a, const mpz_class& b) {
+  mpz_gcd( a.get_mpz_t(), a.get_mpz_t(), b.get_mpz_t() );
+}
 
 /**
- * Standardize a vector of mpz_class integers.
+ * Standardize a vector of integers
  *
  * @param numbers The input vector of integers
  * @post The numbers vector has been reduced by the gcd of its entries
  */
-template<>
-inline mpz_class standardize_vector<mpz_class> ( std::vector<mpz_class>& numbers ) 
-{
-  mpz_class a = numbers[0];
-  for ( const mpz_class& b : numbers ) {
+
+template <typename Number_t, typename Vector_t>
+inline Number_t standardize_vector ( Vector_t& numbers ) {
+  Number_t a = numbers[0];
+  for ( const Number_t& b : numbers ) {
     if ( a == 0 ) {
       if ( b == 0 ) {
         continue;
       }
       a = b;
     }
-    mpz_gcd ( a.get_mpz_t(), a.get_mpz_t(), b.get_mpz_t() );
+    _gcd<Number_t> ( a, b );
     // a = gcd(a,b) > 0
     if ( a == 1 ) {
       return a;
@@ -36,7 +44,7 @@ inline mpz_class standardize_vector<mpz_class> ( std::vector<mpz_class>& numbers
     // NOTE: This is a degenerate case, where all the numbers are 0
     return -1;
   }
-  for ( mpz_class& b : numbers ) { 
+  for ( Number_t& b : numbers ) { 
     b /= a;
   }
   // numbers = numbers / a
@@ -50,7 +58,7 @@ inline mpz_class standardize_vector<mpz_class> ( std::vector<mpz_class>& numbers
  * @post The vector has been reduced to the smallest co-directional integral vector.
  */
 template<>
-inline mpq_class standardize_vector<mpq_class>( std::vector<mpq_class>& numbers ) 
+inline mpq_class standardize_vector<mpq_class, std::vector<mpq_class> >( std::vector<mpq_class>& numbers ) 
 {
   mpz_class a = numbers[0].get_num();
   mpz_class b = numbers[0].get_den();
@@ -75,4 +83,6 @@ inline mpq_class standardize_vector<mpq_class>( std::vector<mpq_class>& numbers 
   return (a / b);
 }
 
-#endif
+} // namespace zonotope
+
+#endif // STANDARDIZE_VECTOR_HPP_
