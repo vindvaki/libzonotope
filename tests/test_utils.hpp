bde159e87ab3dda3eec96718dfcb218dc7beb768
Hörður Freyr Yngvason <hordurf@gmail.com>
2014-05-02 22:50:04 -0700

    Simplified test utils

--- tests/test_utils.hpp
+++ tests/test_utils.hpp
@@ -4,10 +4,11 @@
 #include <vector>
 
 template <typename NT>
-std::vector<std::vector<NT> >
-random_generators (int d, int n,
-                   const NT& lo, const NT& hi,
-                   const NT& seed);
+std::vector<NT>
+random_buffer(typename std::vector<NT>::size_type count,
+              const NT& lo,
+              const NT& hi,
+              const NT& seed);
 
 /*
  * 
@@ -18,24 +19,21 @@ random_generators (int d, int n,
 #include <random>
 
 template<>
-std::vector<std::vector<long> >
-random_generators (int d, int n,
-                   const long& lo, const long& hi,
-                   const long& seed) {
-  std::vector<std::vector<long> > generators (n, std::vector<long> (d));
+std::vector<long>
+random_buffer(typename std::vector<long>::size_type count,
+              const long& lo,
+              const long& hi,
+              const long& seed)
+{
   std::uniform_int_distribution<long> dis (lo, hi);
-  
   std::mt19937 gen(seed);
   
-  for ( int k = 0; k < n; ++k ) {
-    for ( int i = 0; i < d; ++i ) {
-      generators[k][i] = dis(gen);
-    }
+  std::vector<long> result (count);  
+  for ( auto& val : result ) {
+    val = dis(gen);
   }
-
-  return generators;
+  return result;
 }
-
 
 #endif /* TEST_UTILS_HPP_ */
 
