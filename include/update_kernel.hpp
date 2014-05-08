56eedf79632f19771dff836ef609bd1462f284c9
Hörður Freyr Yngvason <hordurf@gmail.com>
2014-05-07 23:01:59 -0700

    Type traits for type dependent kernel updates

--- include/update_kernel.hpp
+++ include/update_kernel.hpp
@@ -1,10 +1,9 @@
-#ifndef UPDATE_KERNEL_HPP_
-#define UPDATE_KERNEL_HPP_
+#ifndef ZONOTOPE_UPDATE_KERNEL_HPP_
+#define ZONOTOPE_UPDATE_KERNEL_HPP_
 
-#include <vector>
-
+#include "type_traits_utils.hpp"
+#include "eigen_utils.hpp"
 #include "standardize_vector.hpp"
-#include "linalg.hpp"
 
 namespace zonotope {
 
@@ -21,51 +20,69 @@ namespace zonotope {
  *       remains untouched.
  */
 
-template <typename Vector_ZZ>
-void update_kernel_zz( std::vector<Vector_ZZ>& kernel, const Vector_ZZ& v )
-{
-  const int d = kernel[0].size();
-  const int k = kernel.size();
 
-  auto x = v;
+template <typename Derived_kernel_ZZ,
+          typename Derived_vector_ZZ>
+enable_if_integer_t<typename Derived_kernel_ZZ::Scalar, 
+                    Derived_col_matrix<Derived_kernel_ZZ> >
+update_kernel(const Eigen::MatrixBase<Derived_kernel_ZZ>& kernel_in,
+              const Eigen::MatrixBase<Derived_vector_ZZ>& v )
+{
+  const int d = kernel_in.rows();
+  const int k = kernel_in.cols();
+
+  Derived_col_matrix<Derived_kernel_ZZ>
+    kernel_out = kernel_in.leftCols(k-1);
+  
+  Derived_row_vector<Derived_kernel_ZZ>
+    x = kernel_in.transpose() * v;
+  
   int j = -1;
   for ( int i = 0; i < k; ++i ) {
-    x[i] = dot( kernel[i], v );
-    if ( x[i] != 0 ) {
+    if ( x(i) != 0 ) {
       j = i;
+      // don't break because we'd like to avoid a swap if possible
     }
   }
 
   if ( j == -1 ) {
     // v is already in the combination
-    return;
+    return kernel_in;
   }
-
-  std::swap(kernel[k-1], kernel[j]);
-  std::swap(x[k-1], x[j]);
-
-  for ( int i = 0; i < k - 1; ++i ) {
-    for ( int r = 0; r < d; ++r ) {
-      kernel[i][r] = x[k-1]*kernel[i][r] - x[i]*kernel[k-1][r];
-    }
-    standardize_vector( kernel[i] );
+  
+  if ( j != k-1 ) {
+    kernel_out.col(j) = kernel_in.col(k-1);
+    std::swap(x(k-1), x(j));
   }
-  kernel.pop_back();
+  
+  for ( int i = 0; i < k-1; ++i ) {
+    kernel_out.col(i) = x(k-1)*kernel_out.col(i) - x(i)*kernel_in.col(j);
+    standardize_vector( kernel_out.col(i) );
+  }
+  
+  return kernel_out;
 }
 
-
-template <typename Vector_FF>
-void update_kernel_ff( std::vector<Vector_FF>& kernel, const Vector_FF& v )
+template <typename Derived_kernel_FF,
+          typename Derived_vector_FF>
+enable_if_not_integer_t<typename Derived_kernel_FF::Scalar, 
+                        Derived_col_matrix<Derived_kernel_FF> >
+update_kernel(const Eigen::MatrixBase<Derived_kernel_FF>& kernel_in,
+                 const Eigen::MatrixBase<Derived_vector_FF>& v)
 {
-  const int d = kernel[0].size();
-  const int k = kernel.size();
+  const int d = kernel_in.rows();
+  const int k = kernel_in.cols();
+  
+  Derived_col_matrix<Derived_kernel_FF>
+    kernel_out = kernel_in.leftCols(k-1);
+  
+  Derived_row_vector<Derived_kernel_FF>
+    x = kernel_in.transpose() * v;
 
-  auto x = v;
   int j = -1;
   for ( int i = 0; i < k; ++i ) {
-    x[i] = dot( kernel[i], v );
-    if ( x[i] != 0 ) {
-      if ( ( j == -1 ) || ( abs(x[j]) > abs(x[i]) ) ) {
+    if ( x(i) != 0 ) {
+      if ( ( j == -1 ) || ( abs(x(j)) > abs(x(i)) ) ) {
         j = i;
       }
     }
@@ -73,22 +90,23 @@ void update_kernel_ff( std::vector<Vector_FF>& kernel, const Vector_FF& v )
 
   if ( j == -1 ) {
     // v is already in the combination
-    return;
+    return kernel_in;
   }
 
-  std::swap(kernel[k-1], kernel[j]);
-  std::swap(x[k-1], x[j]);
+  if ( j != k-1 ) {
+    kernel_out.col(j) = kernel_in.col(k-1);
+    std::swap(x(k-1), x(j));
+  }
 
   for ( int i = 0; i < k - 1; ++i ) {
-    for ( int r = 0; r < d; ++r ) {
-      kernel[i][r] -= (x[i] * kernel[k-1][r]) /x[k-1];
-    }
+    kernel_out.col(i) -= x(i) * kernel_in.col(j) / x(k-1);
   }
-  kernel.pop_back();
+  
+  return kernel_out;
 }
 
 
 
 } // namespace zonotope
 
-#endif // UPDATE_KERNEL_HPP_
+#endif // ZONOTOPE_UPDATE_KERNEL_HPP_
