d574454a03688db36e7799578d7d7d8c4ebc5b0f
Hörður Freyr Yngvason <hordurf@gmail.com>
2014-05-07 23:01:00 -0700

    Type traits for type dependent vector standardization

--- include/standardize_vector.hpp
+++ include/standardize_vector.hpp
@@ -1,8 +1,9 @@
 #ifndef STANDARDIZE_VECTOR_HPP_
 #define STANDARDIZE_VECTOR_HPP_
 
-#include <vector>
-#include <array>
+#include "type_traits_utils.hpp"
+
+#include <eigen3/Eigen/Eigen>
 
 namespace zonotope {
 
@@ -11,45 +12,54 @@ namespace zonotope {
  *
  * @tparam Vector_t An iterble, mutable container of integers of type
  *                  Vector_t::value_type. There must exist a function
- *                  
+ *
  *                     gcd :: T -> T -> T
  *
  *                  for T = Vector_t::value_type, that returns the
  *                  greatest common divisor.
- * 
+ *
  * @param numbers The input vector of integers.
- * 
+ *
  * @post The numbers vector has been reduced by the gcd of its entries
  */
 
-template <typename Vector_t>
-inline typename Vector_t::value_type
-standardize_vector (Vector_t& numbers ) {
-  auto a = numbers[0];
-  
-  for ( const auto& b : numbers ) {
-    if ( b == 0 ) {
+
+template <class Derived>
+enable_if_integer_t<typename Derived::Scalar>
+standardize_vector(Eigen::MatrixBase<Derived> const & numbers_const) {
+  auto& numbers = const_cast< Eigen::MatrixBase<Derived>& >(numbers_const);
+  typename Derived::Scalar a = 0;
+  const int d = numbers.size();
+  for ( int i = 0; i < d; ++i ) {
+    if ( numbers(i) == 0 ) {
       continue;
     }
-    a = gcd(a, b);
- 
+    a = abs(gcd(a, numbers(i)));
     if ( a == 1 ) {
       return a;
     }
   }
-  // a = gcd(numbers)
- 
-  if ( a == 0 ) {
-    // NOTE: This is a degenerate case, where all the numbers are 0
-    return -1;
+
+  if ( a != 0 ) {
+    numbers /= a;
   }
-  for ( auto& b : numbers ) { 
-    b /= a;
-  }
-  
+
   // numbers = numbers / a
   return a;
 }
+
+template <class Derived>
+enable_if_not_integer_t<typename Derived::Scalar>
+standardize_vector (Eigen::MatrixBase<Derived> const & numbers_const) {
+  auto& numbers = const_cast< Eigen::MatrixBase<Derived>& >(numbers_const);
+  auto norm = numbers.template lpNorm<1>();
+  // NOTE: We use the L1 norm (sum of absolute values) to preserve rationality
+  if ( norm > 0 ) {
+    numbers /= norm;
+  }
+  return norm;
+}
+
 
 } // namespace zonotope
 
